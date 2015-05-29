501225bd7d362a0da6c32aff9e90d9044eda9270
maciek <maciej.kaminski@pwr.edu.pl>
2015-05-29 20:16:52 +0200

    Project basic setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pankus',
+    version='0.1',
+    packages=[''],
+    url='http://github.com/mk45/pankus',
+    license='(c) Politechnika Wroclawska',
+    author='Maciej Kaminski',
+    author_email='maciej.kaminski@pwr.edu.pl',
+    description='Spatial planning software'
+)
