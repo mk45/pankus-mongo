873279db84402a12599f1379cf7b61b6b3adeb11
maciek <maciej.kaminski@pwr.edu.pl>
2015-10-09 17:45:45 +0200

    speedup in ram; minor bug update

--- pankus/intopp/make_motion_exchange.py
+++ pankus/intopp/make_motion_exchange.py
@@ -8,6 +8,7 @@ from pankus.storages.ring_total import ring_total
 from pankus.storages.src_dst import src_dst
 from pankus.storages.motion_exchange import motion_exchange
 from pankus.helpers.pbar import Pbar
+from pankus.helpers.ram_collection import RamCollection
 from pankus.defaults.config import \
     sd_id_key,sd_start_key,sd_end_key,ring_key,to_ring_total_key,\
     in_ring_total_key,selectivity_key,sources_key,destinations_key,motion_quantity_key
@@ -22,14 +23,19 @@ def make_motion_exchange():
     """
     pbar = Pbar('make motion exchange: ',src_dst.count())
     motion_exchange.delete_many({})
-    for src_point in src_dst.find():
+
+    ram_src_dst=RamCollection(src_dst)
+    ram_ring=RamCollection(ring,[sd_start_key,sd_end_key])
+    ram_ring_total=RamCollection(ring_total)
+
+    for src_point in ram_src_dst.find():
         pbar.plus_one()
         me = []
-        for dst_point in src_dst.find():
-            r = ring.find_one(
+        for dst_point in ram_src_dst.find():
+            r = ram_ring.find_one(
                 {sd_start_key: src_point[sd_id_key],
                  sd_end_key: dst_point[sd_id_key]})
-            rt = ring_total.find_one(
+            rt = ram_ring_total.find_one(
                 {sd_id_key: src_point[sd_id_key],
                  ring_key: r[ring_key]})
 
@@ -43,8 +49,11 @@ def make_motion_exchange():
             quantity_for_ring =\
                 src_point[sources_key] * (fraction_after_ring - fraction_before_ring)
 
-            quantity_for_dest = quantity_for_ring *\
-                dst_point[destinations_key] / rt[in_ring_total_key]
+            if rt[in_ring_total_key]!=0:
+                quantity_for_dest = quantity_for_ring *\
+                    dst_point[destinations_key] / rt[in_ring_total_key]
+            else:
+                quantity_for_dest = 0
 
             me.append({
                 sd_start_key: src_point[sd_id_key],
