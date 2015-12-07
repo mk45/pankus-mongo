ef44147f9cbf6ac09a9a022cc8c04a26fd7b31d4
maciek <maciej.kaminski@pwr.edu.pl>
2015-12-07 22:09:29 +0100

    voting collection;

---
+++ pankus/storages/voting.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+__author__ = 'Maciej Kamiński Politechnika Wrocławska'
+
+from pankus.database.mongo_collection import MongoCollectionStorageFactory
+from pankus.defaults.config import voting_table_name,sd_start_key,sd_end_key
+
+"""Storage for voting
+
+Module creates object for database collection connection
+
+data:
+    sd_s - startnode id node who votes (indexed field) {0..Z}
+    sd_e - endnode id who is voted(indexed field) {0..Z}
+    satisfied_destinations - quantity of vote (contact volume) between points
+    destinatins - destinations in voting
+    in_ring_total
+    to_ring_total
+    ring
+
+
+"""
+
+voting = MongoCollectionStorageFactory(voting_table_name,[sd_start_key,sd_end_key])
