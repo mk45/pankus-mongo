960b6668e64db5c8cdae719179178dab6694f72e
maciek <maciej.kaminski@pwr.edu.pl>
2016-01-05 17:12:40 +0100

    storage for withdrawal excess;

---
+++ pankus/storages/motion_exchange_withdrawal_excess.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+__author__ = 'Maciej Kamiński Politechnika Wrocławska'
+
+from pankus.database.mongo_collection import MongoCollectionStorageFactory
+from pankus.defaults.config import \
+    sd_start_key,sd_end_key,motion_exchange_withdrawal_excess_name
+
+"""Storage for ring
+
+Module creates object for database collection connection
+
+data:
+    sd_s - startnode id (indexed field) {0..Z}
+    sd_e - endnode id (indexed field) {0..Z}
+    q - quantity of motion (contact volume) between points
+"""
+
+motion_exchange_withdrawal_excess =\
+    MongoCollectionStorageFactory(motion_exchange_withdrawal_excess_name,[sd_start_key,sd_end_key])
