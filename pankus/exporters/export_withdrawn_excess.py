3e9b811a8a3819bd18e6cc0a86882804dd091ee3
maciek <maciej.kaminski@pwr.edu.pl>
2016-01-07 16:59:04 +0100

    add sources destinations;

--- pankus/exporters/export_withdrawn_excess.py
+++ pankus/exporters/export_withdrawn_excess.py
@@ -3,10 +3,13 @@
 
 from pankus.storages.motion_exchange_withdrawal_excess import motion_exchange_withdrawal_excess
 from pankus.storages.src_dst import src_dst
+from pankus.storages.sd_point import sd_point
+
 from pankus.storages.ring import ring
 from pankus.helpers.ram_collection import RamCollection
 from pankus.helpers.pbar import Pbar
-from pankus.defaults.config import ring_key,sd_id_key,sd_start_key,sd_end_key,motion_quantity_key
+from pankus.defaults.config import ring_key,sd_id_key,sd_start_key,\
+    sd_end_key,motion_quantity_key,destinations_key
 import csv
 
 
@@ -15,6 +18,7 @@ def export_withdrawn_excess():
     ram_src_dst=RamCollection(src_dst)
     ram_motion_exchange_withdrawal_excess=RamCollection(motion_exchange_withdrawal_excess)
     ram_ring=RamCollection(ring,[sd_start_key,sd_end_key])
+    ram_sd_point=RamCollection(sd_point,[sd_id_key])
 
     output=[]
     pbar = Pbar('exporting: ',ram_src_dst.count())
@@ -23,6 +27,7 @@ def export_withdrawn_excess():
         for region_end in ram_src_dst.find():
 
             output.append({
+
                 sd_start_key:region_start[sd_id_key],
                 sd_end_key:region_end[sd_id_key],
                 motion_quantity_key:ram_motion_exchange_withdrawal_excess.find_one({
@@ -32,9 +37,12 @@ def export_withdrawn_excess():
                 ring_key:ram_ring.find_one({
                     sd_start_key:region_start[sd_id_key],
                     sd_end_key:region_end[sd_id_key]
-                })[ring_key]
+                })[ring_key],
+                destinations_key:ram_sd_point.find_one({
+                    sd_id_key:region_end[sd_id_key]
+                })[destinations_key]
             })
-    fieldnames=[sd_start_key,sd_end_key,ring_key,motion_quantity_key]
+    fieldnames=[sd_start_key,sd_end_key,ring_key,destinations_key,motion_quantity_key]
     with open('out_we_mx.csv','wb') as f:
         writer=csv.DictWriter(f,fieldnames=fieldnames)
         writer.writeheader()
