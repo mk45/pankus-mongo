387f08d7a6b8fdd29d55158d224731e51f776b22
maciek <maciej.kaminski@pwr.edu.pl>
2016-04-12 11:40:39 +0200

    New convolution model base structures in database;

---
+++ pankus/storages/motion_exchange_convolution.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+__author__ = 'Maciej Kamiński Politechnika Wrocławska'
+
+from pankus.database.mongo_collection import MongoCollectionStorageFactory
+from pankus.defaults.config import \
+    sd_start_key,sd_end_key,motion_exchange_convolution_name
+
+"""Storage for ring
+
+Module creates object for database collection connection
+
+data:
+    sd_s - startnode id (indexed field) {0..Z}
+    sd_e - endnode id (indexed field) {0..Z}
+    q - quantity of motion (contact volume) between points
+"""
+
+motion_exchange_convolution = MongoCollectionStorageFactory(motion_exchange_convolution_name,[sd_start_key,sd_end_key])
