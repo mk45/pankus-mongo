b5fb4dee5c51fc65d83886b3032ac347af6bb205
maciek <maciej.kaminski@pwr.edu.pl>
2016-04-19 15:55:53 +0200

    Example to draw convolution DCF

---
+++ examples/other/testcdf.py
@@ -0,0 +1,17 @@
+from pankus.intopp.make_motion_exchange_convolution import convolution_cdf
+import matplotlib.pyplot as plt
+import argparse
+import sys
+
+selectivity=float(sys.argv[1])
+conv_a=float(sys.argv[2])
+conv_b=float(sys.argv[3])
+steps=int(sys.argv[4])
+
+x=range(steps*10)
+
+f=lambda s: convolution_cdf(s/10.0,selectivity,conv_a,conv_b)
+
+plt.plot(x,map(f,x))
+plt.show()
+
