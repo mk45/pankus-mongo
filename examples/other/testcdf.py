0ba67c9a2cbd30597b903dcf4fbc5c67a11b768b
maciek <maciej.kaminski@pwr.edu.pl>
2016-04-22 15:23:59 +0200

    bugfix;

--- examples/other/testcdf.py
+++ examples/other/testcdf.py
@@ -8,7 +8,12 @@ conv_a=float(sys.argv[2])
 conv_b=float(sys.argv[3])
 steps=int(sys.argv[4])
 
-x=[t/10.0 for t in range(steps*10)]
+try:
+    minimum=int(sys.argv[5])
+except:
+    minimum=0
+
+x=[(t-minimum)/10.0 for t in range(steps*10)]
 
 f=lambda s: convolution_cdf(s,selectivity,conv_a,conv_b)
 
