7589138d3ecc173b14eea555938679a97c2ea879
maciek <maciej.kaminski@pwr.edu.pl>
2016-04-25 15:55:33 +0200

    add example and cdf draw functions;

--- examples/other/testcdf.py
+++ examples/other/testcdf.py
@@ -1,22 +1,30 @@
-from pankus.intopp.make_motion_exchange_convolution import convolution_cdf
+from pankus.intopp.make_motion_exchange import convolution_mix
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import argparse
 import sys
 
-selectivity=float(sys.argv[1])
-conv_a=float(sys.argv[2])
-conv_b=float(sys.argv[3])
-steps=int(sys.argv[4])
+
+steps=int(sys.argv[1])
+selectivity=float(sys.argv[2])
+conv_a=float(sys.argv[3])
+conv_b=float(sys.argv[4])
+alpha=float(sys.argv[5])
+
 
 try:
-    minimum=int(sys.argv[5])
+    filename=str(sys.argv[6])
+    image=mpimg.imread(filename)
+
 except:
-    minimum=0
+    # white as background image
+    image=[[[1.0,1.0,1.0]]]
 
-x=[(t-minimum)/10.0 for t in range(steps*10)]
+x=[(t-0.0)/10.0 for t in range(steps*10)]
 
-f=lambda s: convolution_cdf(s,selectivity,conv_a,conv_b)
+f=lambda s: convolution_mix(s,selectivity,conv_a,conv_b,alpha)
 
-plt.plot(x,map(f,x))
+plt.plot(x,map(f,x),zorder=1)
+plt.imshow(image,zorder=0,extent=[0.0,steps,0.0,1.0],aspect='auto')
 plt.show()
 
